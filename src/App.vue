f5297d1b1f62298217af9d427c0b7009bd06e980
jeffting <jefftingey22@gmail.com>
2019-07-06 13:58:33 -0600

    carousel is working

--- src/App.vue
+++ src/App.vue
@@ -12,19 +12,12 @@ export default {
 <style lang="less">
 @import "./assets/css/styles";
 
-body {
-  // background-color: @main-background-color !important;
-}
-
 #app {
   font-family: "Avenir", Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
-  /* text-align: center; */
   padding-left: 15%;
   padding-right: 15%;
-  color: #2c3e50;
   padding-top: 90px;
-  /* background-color: black; */
 }
 </style>
--- src/components/Home.vue
+++ src/components/Home.vue
@@ -1,16 +1,36 @@
 <template>
   <div>
-    <img src="../assets/jeff.png" class="img-fluid" alt="picture of jeff">
-    <div class="text-div text">Thank you for visiting my page</div>
+    <carousel></carousel>
+    <h2 class="text title-header">Thanks for visiting my page!</h2>
+    <div class="text">
+      My name is Jeff Tingey. I am a software developer, studying Computer Science at Brigham Young University.
+      <br>Please explore my page or view my resume!
+    </div>
+    <button v-on:click="openPdf">Resume</button>
   </div>
 </template>
-<script></script>
+<script>
+import Carousel from "./Carousel";
+export default {
+  components: { Carousel },
+  methods: {
+    openPdf() {
+      // console.log("here");
+      window.open("../assets/nov14resume.pdf", "_blank", "fullscreen=yes");
+    }
+  }
+};
+</script>
 <style lang="less">
 @import "../assets/css/styles";
-
+.title-header {
+  margin-top: 50px;
+}
+.hooper {
+  height: auto;
+}
 .img-fluid {
   height: 300px;
-  float: left;
 }
 .text-div {
   font-size: 50px;
